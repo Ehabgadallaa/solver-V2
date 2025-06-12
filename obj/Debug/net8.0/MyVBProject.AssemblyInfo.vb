3bab4faf0df80e5e6a67d402ef6b3e19692b1ef1
ehab gadallaa <ehab.gadallaa@lafarge.com>
2025-06-12 13:28:42 +0300

    V2

--- obj/Debug/net8.0/MyVBProject.AssemblyInfo.vb
+++ obj/Debug/net8.0/MyVBProject.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("MyVBProject"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c429dbae871dc832073e7b5f1d2a8a86a972d540"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ac9c22e070e7454dc292f8afea82904a0ad22e"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("MyVBProject"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("MyVBProject"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
